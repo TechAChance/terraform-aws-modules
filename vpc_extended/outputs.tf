6047ad36b1b2f6bc6861baa95cc23ad3c91be865
SnideB0t <snideb0t@techachance.com>
2021-11-30 20:30:30 +0100

    Fix outputs

---
+++ vpc_extended/outputs.tf
@@ -0,0 +1,45 @@
+## VPC
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+## Subnets
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "database_subnets" {
+  description = "List of IDs of database subnets"
+  value       = module.vpc.database_subnets
+}
+
+# # NAT gateways
+# output "nat_public_ips" {
+#   description = "List of public Elastic IPs created for AWS NAT Gateway"
+#   value       = module.vpc.nat_public_ips
+# }
+
+## Security Groups
+output "public_sg" {
+  description = "Id of public security group."
+  value       = module.security_group_public.security_group_id
+}
+output "private_sg" {
+  description = "Id of private security group."
+  value       = module.security_group_private.security_group_id
+}
+output "database_sg" {
+  description = "Id of database security group"
+  value       = module.security_group_database.security_group_id
+}
+output "bastion_sg" {
+  description = "Id of bastion security group"
+  value       = module.security_group_bastion.security_group_id
+}
