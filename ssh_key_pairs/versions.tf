44c9713ea6a0125c458900f65e7e645e5fcef204
SnideB0t <snideb0t@techachance.com>
2021-12-01 14:24:38 +0100

    Add common versions.tf

--- ssh_key_pairs/versions.tf
+++ ssh_key_pairs/versions.tf
@@ -1,13 +1,1 @@
-################
-# VERSIONS     #
-################
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.65"
-    }
-  }
-}
+../_shared_/versions.tf
