bc382d818c86026cb0a01fd6826709897387a2af
SnideB0t <snideb0t@techachance.com>
2022-08-22 13:00:46 +0200

    Add IAM SMTP user module

---
+++ iam_smtp_user/output.tf
@@ -0,0 +1,8 @@
+output "smtp_username" {
+  value = aws_iam_access_key.smtp_user.id
+}
+
+output "smtp_password" {
+  value     = aws_iam_access_key.smtp_user.ses_smtp_password_v4
+  sensitive = true
+}
